45370c359aa6f70316e201f68e2dec042bac78c5
khomabhena <colwanymab@gmail.com>
2023-07-08 12:52:39 +0200

    changed fecth my data url to live url

--- fetch/fetchMyData.js
+++ fetch/fetchMyData.js
@@ -1,5 +1,5 @@
-const url = 'http://localhost:3030'
-// const url = 'https://imali-app-server.onrender.com'
+// const url = 'http://localhost:3030'
+const url = 'https://imali-app-server.onrender.com'
 
 export const fetchMyData = async (route, params) => {
     const response = await fetch(`${url}${route}` + '?' + new URLSearchParams(
